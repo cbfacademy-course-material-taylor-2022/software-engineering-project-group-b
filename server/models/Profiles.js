bca35b08a7156f5b7babf11c2ac260b3728a004b
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-17 17:13:38 +0000

    add boilerplate code reset

--- server/models/Profiles.js
+++ server/models/Profiles.js
@@ -1,14 +1,11 @@
-const mongoose = require("mongoose");
 
-const { Schema } = mongoose;
+ const mongoose = require("mongoose");
+ const { Schema } = mongoose;
 
-const profileSchema = new Schema({
-  first_name: String,
-  last_name: String,
-  location: String,
-});
+ const profileSchema = new Schema({
+   first_name: String,
+   last_name: String,
+   location: String,
+ });
 
-const ProfilesModel = mongoose.model("profiles", profileSchema);
-
-
-module.exports = ProfilesModel;
+ mongoose.model("profiles", profileSchema);
