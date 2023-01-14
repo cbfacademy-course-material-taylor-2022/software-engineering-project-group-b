b8a245ef9154456200eeb884194e2a4325a4ef31
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-01-14 11:26:35 +0000

    add form page and image

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,90 @@
-import React, { useState, useEffect } from "react";
-// import Navbar from './components/Navbar.js'
-import NavbarSmall from './components/NavbarSmall.js'
 
- // SERVICES THAT CALL OUR API ENDPOINTS
- import { getAllProfiles } from "./services/profileService";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Signature from './Signature'
 
- function App() {
-   const [profiles, setProfiles] = useState(null);
 
-   useEffect(() => {
-     async function getProfiles() {
-       if (!profiles) {
-         const response = await getAllProfiles();
-         setProfiles(response);
-       }
-     }
+const App = () => {
+  return (
+  <Router>
+        <Routes>
+          <Route path="/" element={<Signature />}/>
 
-     getProfiles();
-   }, [profiles]);
+        </Routes>
+  </Router>
+  );
 
-   const renderProfile = (user) => {
-     return (
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// // import Navbar from './components/Navbar.js'
+// import NavbarSmall from './components/NavbarSmall.js'
+// import onBoarding from './onBoarding.js'
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//  // SERVICES THAT CALL OUR API ENDPOINTS
+//  import { getAllProfiles } from "./services/profileService";
+
+//  function App() {
+//    const [profiles, setProfiles] = useState(null);
+
+//    useEffect(() => {
+//      async function getProfiles() {
+//        if (!profiles) {
+//          const response = await getAllProfiles();
+//          setProfiles(response);
+//        }
+//      }
+
+//      getProfiles();
+//    }, [profiles]);
+
+//    const renderProfile = (user) => {
+//      return (
     
     
-       <li key={user._id}>
-         <h3>
-           {`${user.first_name} 
-           ${user.last_name}`}
-         </h3>
-         <p>{user.location}</p>
-       </li>
+//        <li key={user._id}>
+//          <h3>
+//            {`${user.first_name} 
+//            ${user.last_name}`}
+//          </h3>
+//          <p>{user.location}</p>
+//        </li>
       
-     );
-   };
+//      );
+//    };
 
-   return (
-     <div>
-        {/* <Navbar/> */}
-        <NavbarSmall/>
-       <ul>
-         {profiles && profiles.length > 0 ? (
-           profiles.map((profile) => renderProfile(profile))
-         ) : (
-           <p>No profiles found</p>
-         )}
-       </ul>
+//    return (
+//      <div>
+//         {/* <Navbar/> */}
+//         <NavbarSmall/>
+//         <onBoarding/>
+//        <ul>
+//          {profiles && profiles.length > 0 ? (
+//            profiles.map((profile) => renderProfile(profile))
+//          ) : (
+//            //<p>No profiles found</p>
+
+//            <BrowserRouter>
+//            <Route exact path="/" render={()=>(<onBoarding/>)}/>
+//            </BrowserRouter>
+//          )}
+//        </ul>
        
-     </div>
+//      </div>
      
-   );
- }
+//    );
+ 
+//  }
 
- export default App;
+//  export default App;
---
+++ client/src/Signature.js
@@ -0,0 +1,126 @@
+
+//import Navbar from './components/Navbar.js'
+import NavbarSmall from './components/NavbarSmall.js'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import './signature.css'
+import image from './images/boywithtray.jpg';
+
+
+ // SERVICES THAT CALL OUR API ENDPOINTS
+//  import { getAllProfiles } from "./services/profileService";
+
+const Signature= () => {
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        location: ""
+    })
+
+    let navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        console.log('submission')
+        e.preventDefault()
+        try{
+            const response = await axios.put('http:localhost:8080/profiles', {formData})
+            console.log(response)
+            const success = response.status === 2000
+            if (success) navigate('/')
+            //add path above
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleChange = (e) => {
+        console.log('e', e)
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+        const name = e.target.name
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+   return (
+    <>
+    <NavbarSmall
+        minimal={true}
+        setShowModal={() => {
+
+        }}
+        showModal={false}
+    
+    />
+   
+       
+        <div className="signature">
+               <h2>FREE SCHOOL DINNERS</h2>
+               <h2>PETITION.</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                        id="first_name"
+                        type='text'
+                        name="first_name"
+                        placeholder="First Name"
+                        required={true}
+                        value={formData.first_name}
+                        onChange={handleChange}
+                    />
+
+                <label htmlFor="last_name">Last Name</label>
+                    <input
+                    id="last_name"
+                    type='text'
+                    name="last_name"
+                    placeholder="Last Name"
+                    required={true}
+                    value={formData.last_name}
+                    onChange={handleChange}
+
+                />
+
+                <label htmlFor="email">Email</label>
+                    <input
+                    id="email"
+                    type='text'
+                    name="email"
+                    placeholder="Email"
+                    required={true}
+                    value={formData.email}
+                    onChange={handleChange}
+
+                />
+
+                <label htmlFor="location">Location</label>
+                    <input
+                    id="location"
+                    type='text'
+                    name="location"
+                    placeholder="Location"
+                    required={true}
+                    value={formData.location}
+                    onChange={handleChange}
+                />  
+
+                    <div className="photo-container"><img src={image} alt="meals" width={500}/>
+
+                    </div>
+         
+                </section>
+            </form>
+        </div>
+        </>   
+        )
+
+   }
+
+export default Signature
