bf524afc64c647acc6785deaea369ae76bc7d748
NAW125 <waugh.natalie@gmail.com>
2023-01-18 09:13:17 +0400

    updates to homepage text and layout

---
+++ client/src/pages/Home.js
@@ -0,0 +1,30 @@
+import Navbar from '../components/Navbar';
+import { useState } from 'react';
+
+
+const Home = () => {
+
+    //handle click fuction/* 
+}
+
+return (
+    
+    <div>
+        <h1> FREE SCHOOL DINNERS </h1>
+        <h1> FOR EVERY CHILD </h1>
+        <p> When children eat well, their health and educational outcomes improve. 
+            Food eaten during the school day makes up around 17% of the average 4-10-year old’s daily meal consumption. 
+            Therefore, ensuring the food children eat at school is healthy and nutritious is critical.
+            Making school meals free for all primary children in England could play an important role in decreasing 
+            obesity prevalence and closing the educational attainment gap in the UK. 
+        </p>
+    </div>
+    
+    )
+let btn = document.createElement ('button'),
+btn.innerHTML = 'Sign the Petiton';
+btn.type = 'submit';
+btn.name = 'formBtn';
+document.body.appendChild(btn);
+
+};
