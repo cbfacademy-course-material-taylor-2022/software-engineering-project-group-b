8b4b1b5195fa078231d867609723688fc7ad59ea
Maryanali <73916090+Maryanali@users.noreply.github.com>
2023-01-21 14:19:34 +0000

    Image in signature page is responsive

--- client/src/profile.js
+++ client/src/profile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 // import { useNavigate } from 'react-router-dom'
 // import axios from 'axios'
 import Navbar from "./components/Navbar.js";
-import "./profile.css";
+import "./Profile.css";
 // import './signature.css'
 import "./App.css";
 // import NavbarSmall from './components/NavbarSmall.js'
