41140ccdd2caaa1b17c5ccc8442612371d7c9135
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-01-28 13:42:31 +0000

    jest test working on navbar

--- client/src/components/Navbar.test.js
+++ client/src/components/Navbar.test.js
@@ -4,9 +4,10 @@ import Navbar from './Navbar';
 
 describe('Navbar', () => {
     it('renders expected elements', () => {
-        const navbar = mount(<Navbar/>);
-        var image = navbar.find('img');
-    expect(image).toBeInTheDocument();
+        const navbar = render(<Navbar/>);
+        expect(document.querySelectorAll('img')).toHaveLength(1);
+    //     var image = navbar.find('img');
+    // expect(image).toBeInTheDocument();
     // expect(container.firstChild).toBeDefined();
     
     });
