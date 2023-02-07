ec15117a5a1381edf5506137b85eccf9850b2250
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-02-07 19:52:35 +0000

    added name

--- client/src/Thanks.js
+++ client/src/Thanks.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
+import {useLocation, useNavigate} from 'react-router-dom';
 import Navbar from "./components/Navbar.js";
 import "./Thanks.css";
 import "./App.css";
 import image from "./images/kids_with_hands_up.jpg";
 
+
 const Thanks = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate('/profile')
+  }, 5000)
+
+
   return (
     <div>
       <div className="overlay">
@@ -23,7 +32,7 @@ const Thanks = () => {
               <span className="yellow-border">THANKS</span>
             </h1>
             <h1>
-              <span className="yellow-border2">DESTINY,</span>
+              <span className="yellow-border2">{location.state.first_name},</span>
             </h1>
             <h1>
               <span className="yellow-border2">FOR GIVING</span>
