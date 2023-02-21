530f899b0c80397289536e7044f59a665355c934
Maryanali <73916090+Maryanali@users.noreply.github.com>
2023-02-21 00:07:42 +0000

    more testing on signature and profile

--- client/src/Thanks.test.js
+++ client/src/Thanks.test.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 import Thanks from "./Thanks";
 
-it("renders Thank you message", () => {
-  render(<Thanks />); //thanks
-  expect(screen.getByText("THANKS")).toBeInTheDocument();
+// it("renders Thank you message", () => {
+//   render(<Thanks />); //thanks
+//   expect(screen.getByText("THANKS")).toBeInTheDocument();
+// });
+
+// test("renders THANKS component", async () => {
+//   render(<Thanks />);
+
+//   expect(screen.getByRole("yellow-border")).toBeInTheDocument();
+// });
+
+it("renders welcome message", () => {
+  render(<Thanks />);
+  expect(screen.getByText("HELLO")).toBeInTheDocument();
 });
--- client/src/signature.test.js
+++ client/src/signature.test.js
@@ -1,26 +1,29 @@
-import react from 'react';
-import { render } from '@testing-library/react';
-import Signature from './Signature';
-import { BrowserRouter } from 'react-router-dom';
+import react from "react";
+import Signature from "./Signature";
+import { BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
 
-describe('Signature', () => {
-    // xit('renders expected elements', () => {
-    //     const signature = render(<Signature />);
-    //     expect(document.querySelectorAll('img')).toHaveLength(1);
-    // });
+//describe('Signature', () => {
+// xit('renders expected elements', () => {
+//     const signature = render(<Signature />);
+//     expect(document.querySelectorAll('img')).toHaveLength(1);
+// });
 
+//it('renders expected elements', () => {
+//const signature = render(<BrowserRouter> <Signature /> </BrowserRouter>);
+// expect(shallow(
+//     <BrowserRouter>
+//       <Signature />
+//     </BrowserRouter>
+//   ).length).toEqual(1);
 
-    it('renders expected elements', () => {
-        const signature = render(<BrowserRouter> <Signature /> </BrowserRouter>);
-        // expect(shallow(
-        //     <BrowserRouter>
-        //       <Signature />
-        //     </BrowserRouter>
-        //   ).length).toEqual(1);
-          
-        expect(document.querySelectorAll('h2')).toHaveLength(2);
-    
-    });
+//expect(document.querySelectorAll('h2')).toHaveLength(2);
 
+//});
+
+//});
+
+it("renders welcome message", () => {
+  render(<Signature />);
+  expect(screen.getByText("First Name")).toBeInTheDocument();
 });
-
